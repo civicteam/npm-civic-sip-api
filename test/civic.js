475f4260d18ac6bcfac5a88e971e61480a2f6b99
IainFra <Iain.Fra@gmail.com>
2017-05-15 00:44:29 -0700

    Update civic.js

--- test/civic.js
+++ test/civic.js
@@ -12,8 +12,8 @@ describe('exchangeCode', function() {
 
   const authCode = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NksifQ.eyJqdGkiOiI1Y2QxY2RiMS05NWRkLTQ5MWYtODE4Mi1mZTdkNmE1NmEzZjciLCJpYXQiOjE0OTQ3MDU2NzAuNzYzLCJleHAiOjE0OTQ3MDU4NTAuNzYzLCJpc3MiOiJjaXZpYy1zaXAtaG9zdGVkLXNlcnZpY2UiLCJhdWQiOiJodHRwczovL2FwaS5jaXZpYy5jb20vc2lwLyIsInN1YiI6ImJiYjEyMyIsImRhdGEiOnsiY29kZVRva2VuIjoiNWVhNjkwN2EtMTQ0MS00NTIwLWFlYmItYjIwOTQ1NjYwM2I2In19.Ih5n-CuzbwcpfOFVYp13UBCyATFsxt52OUl8cvkEvQgU7dQ_UzISnXV30WdFTooHpW9as8uhMeBG3IXTJzktxQ';
   const civicClient = civicSip.newClient({
-    appId: 'aaa123', // insert appId
-    appSecret: '7cf5ac70fc9eb1671c85547ef594599ce8214e0c6563e12f24cbd338b8e649c4',
+    appId: '123123ert', // insert appId
+    appSecret: '***',
     env: 'dev',
   });
 
