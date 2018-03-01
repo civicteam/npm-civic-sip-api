e2c636a67a434043177614332bbc49e89e69f645
Stephen Smit <stephen@civic.com>
2018-03-01 13:35:11 -0800

    Updated tests to be long living

--- test/civic.js
+++ test/civic.js
@@ -1,8 +1,8 @@
 const needle = require('needle');
 const civicSip = require('../index');
-const { assert } = require('chai');
-
 const nock = require('nock');
+const sinon = require('sinon');
+const jwtjs = require('../lib/jwt');
 
 const HEX_PRVKEY_NIST = 'bf5efd7bdde29dc28443614bfee78c3d6ee39c71e55a0437eee02bf7e3647721';
 // const HEX_PUBKEY_NIST = '047d9fd38a4d370d6cff16bf12723e343090d475bf36c1d806b625615a7873b0919f131e38418b0cd5b8a3e0a253fe3a958c7840bfc6be657af68062fecd7943d1';
@@ -11,7 +11,7 @@ const SECRET = '44bbae32d1e02bf481074177002bbdef';
 describe('Civic SIP Server', function test() {
   this.timeout(10000);
 
-  const API = 'https://kw9lj3a57c.execute-api.us-east-1.amazonaws.com/';
+  const API = 'https://kw9lj3a57c.execute-api.us-east-1.amazonaws.com';
   const STAGE = 'dev';
   const authCode = 'eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIxNzc1ZDQwMi05ZjNjLTQ0OWUtYWZkYS04ZDk4MmM0OGIxYjIiLCJpYXQiOjE1MTk5MzE3MTcuMDM1LCJleHAiOjE1MTk5MzM1MTcuMDM1LCJpc3MiOiJjaXZpYy1zaXAtaG9zdGVkLXNlcnZpY2UiLCJhdWQiOiJodHRwczovL2FwaS5jaXZpYy5jb20vc2lwLyIsInN1YiI6Ikh5aGFXTzFTRyIsImRhdGEiOnsiY29kZVRva2VuIjoiYTRhYjE1MDEtZTg0Ni00NmUyLWEwZDktMzEyNTAwNmIxNzUzIn19.1d3Q3QeL8SE_wlyxHPi6Pn-buf8XsxRlCkfhULiI5CbDLCgEjLuVMGIFSUXg6_snXOD9p-ImVml-0yF-A2-qaw';
   const returnData = 'eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI1OTYzNWQ2Yy0zYzUyLTQwMzktOTg2OS05MWQwMjUzN2M2YjIiLCJpYXQiOjE1MTk5MzI1NzIuMTU4LCJleHAiOjE1MTk5MzQzNzIuMTU4LCJpc3MiOiJjaXZpYy1zaXAtaG9zdGVkLXNlcnZpY2UiLCJhdWQiOiJodHRwczovL2FwaS5jaXZpYy5jb20vc2lwLyIsInN1YiI6Ikh5aGFXTzFTRyIsImRhdGEiOiI0MDNkNjI0MzY1OTYwMjIyYmQzMWE2MWNhMjQzNWYyY1dOWjhrWkNEUWZWQmtSSVdsbDkzNGhZbDRUTGlrWWVENU52WE0xTUowN2FVQzFtcnFmdVdoWk5qQWVKT1plS0M2emk5Umh3cWR0bkswdWxNRFAwTkRaTHBRa2JqaVdBb1c5RXFYQW41eHNyemZSNUZ0cXZqZ0NORzNvUkp0Y29tRVBvaGVWMDZ3NWZDQ0Z1TjQrbTNiSW5CNldMamNBSmVObUJZT2oyWjFFQVoxcHZ0R2RwSThMWTVYS2VFTHpKM3MzZndidEpXbkorSHFqakxsQjJPM0lmaDBRdVdUMldUNWVrc3RLN1F1bk5MSldiSzJqWkkveGc0RHJFWFl0dnEifQ.YBBljiXaqrbiftAhu6X6csDVbRLcsSNf3xZNRgQzj6Wd7v1Ilja55H_K_gO7zFzj3Qi-bc7-83SI1w6A4Y7MEA';
@@ -37,7 +37,28 @@ describe('Civic SIP Server', function test() {
       },
     };
 
-    const url = `${API}${STAGE}/scopeRequest/authCode`;
+    const url = `${API}/${STAGE}/scopeRequest/authCode`;
+
+    nock(`${API}:443`, { encodedQueryParams: true })
+      .post(`/${STAGE}/scopeRequest/authCode`, { authToken: authCode })
+      .reply(401, 'Unauthorized', ['Content-Type',
+        'application/json',
+        'Content-Length',
+        '12',
+        'Connection',
+        'close',
+        'Date',
+        'Thu, 01 Mar 2018 21:12:30 GMT',
+        'x-amzn-RequestId',
+        '4061216b-1d95-11e8-907a-a10c87994583',
+        'X-Amzn-Trace-Id',
+        'sampled=0;root=1-5a986cbe-fb795596a9eadaeb13e9cc63',
+        'X-Cache',
+        'Error from cloudfront',
+        'Via',
+        '1.1 69ecfaf49062e67077b5f6c4aaf1881f.cloudfront.net (CloudFront)',
+        'X-Amz-Cf-Id',
+        'iD-SPGUuIGViCT28gs0j2Nhk3Rz_NkJ7sHGiEov0H2aeBkfdq2kMrg==']);
 
     needle.post(url, JSON.stringify(body), options, (err, resp) => {
       if (err) {
@@ -54,8 +75,10 @@ describe('Civic SIP Server', function test() {
   it('should exchange authCode for user data.', (done) => {
     const doneFn = done;
 
-    nock('https://kw9lj3a57c.execute-api.us-east-1.amazonaws.com:443', { encodedQueryParams: true })
-      .post('/dev/scopeRequest/authCode', { authToken: authCode })
+    sinon.stub(jwtjs, 'verify').returns(true);
+
+    nock(`${API}:443`, { encodedQueryParams: true })
+      .post(`/${STAGE}/scopeRequest/authCode`, { authToken: authCode })
       .reply(200, {
         data: returnData, userId: '0eb98e188597a61ee90969a42555ded28dcdddccc6ffa8d8023d8833b0a10991', encrypted: true, alg: 'aes',
       }, ['Content-Type',
@@ -83,10 +106,11 @@ describe('Civic SIP Server', function test() {
 
     civicClient.exchangeCode(authCode).then((data) => {
       console.log(data);
-      nock.restore();
+      jwtjs.verify.restore();
       doneFn();
     })
       .catch((error) => {
+        jwtjs.verify.restore();
         doneFn(error);
       });
   });
