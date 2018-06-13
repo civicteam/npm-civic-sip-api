1337874d32823b8a0b24d14dd50314d13a5a8408
Donavan Lawson <donavan@nonacreative.com>
2018-06-13 12:37:34 +0200

    linting rules and airbnb-base

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,78 +12,19 @@ module.exports = {
       objectLiteralDuplicateProperties: false
     }
   },
-
+  extends: ['airbnb-base'],
+  plugins: [
+    "no-only-tests"
+  ],
   rules: {
-    'max-len': ['warn', 200],
-    'no-plusplus': 'warn',
-    'arrow-body-style': ['error', 'as-needed', {
-      requireReturnForObjectLiteral: false,
-    }],
-    'arrow-parens': ['error', 'as-needed', {
-      requireForBlockBody: true,
-    }],
-    'arrow-spacing': ['error', { before: true, after: true }],
-    'constructor-super': 'error',
-    'generator-star-spacing': ['error', { before: false, after: true }],
-    'no-class-assign': 'error',
-    'no-confusing-arrow': ['error', {
-      allowParens: true,
-    }],
-    'no-const-assign': 'error',
-    'no-dupe-class-members': 'error',
-    'no-duplicate-imports': 'off',
-    'no-new-symbol': 'error',
-    'no-restricted-imports': ['off', {
-      paths: [],
-      patterns: []
-    }],
-    'no-this-before-super': 'error',
-    'no-useless-computed-key': 'error',
-    'no-useless-constructor': 'error',
-    'no-useless-rename': ['error', {
-      ignoreDestructuring: false,
-      ignoreImport: false,
-      ignoreExport: false,
-    }],
-    'no-var': 'error',
-    'object-shorthand': ['error', 'always', {
-      ignoreConstructors: false,
-      avoidQuotes: true,
-    }],
-    'prefer-arrow-callback': ['error', {
-      allowNamedFunctions: false,
-      allowUnboundThis: true,
-    }],
-    'prefer-const': ['error', {
-      destructuring: 'any',
-      ignoreReadBeforeAssign: true,
-    }],
-    'prefer-destructuring': ['error', {
-      VariableDeclarator: {
-        array: false,
-        object: true,
-      },
-      AssignmentExpression: {
-        array: true,
-        object: true,
-      },
-    }, {
-      enforceForRenamedProperties: false,
-    }],
-    'prefer-numeric-literals': 'error',
-    'prefer-reflect': 'off',
-    'prefer-rest-params': 'error',
-    'prefer-spread': 'error',
-    'prefer-template': 'error',
-    'require-yield': 'error',
-    'rest-spread-spacing': ['error', 'never'],
-    'sort-imports': ['off', {
-      ignoreCase: false,
-      ignoreMemberSort: false,
-      memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
-    }],
-    'symbol-description': 'error',
-    'template-curly-spacing': 'error',
-    'yield-star-spacing': ['error', 'after']
+    "max-len": ["warn", 200],
+    "import/no-extraneous-dependencies": ["error", { "devDependencies": ["**/test/**/*.js"]}],
+    "no-plusplus": "warn",
+    "consistent-return": "warn",
+    "no-template-curly-in-string": "warn",
+    "comma-dangle": "warn",
+    "import/no-unresolved": "warn",
+    "no-only-tests/no-only-tests": 2
   }
 };
+
