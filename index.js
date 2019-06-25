f80ef0c216ded61bf1812151d53edf9cdf228279
kevinhcolgan <37535663+kevinhcolgan@users.noreply.github.com>
2019-06-25 11:39:01 +0200

    Hotfix/re revert allow s3 uploaded payload (#44)
    * commented external processing flag
    
    * re-added allowS3UploadedPayload

--- index.js
+++ index.js
@@ -196,7 +196,9 @@ sipClientFactory.newClient = (configIn) => {
    *
    */
   const exchangeCode = (jwtToken) => {
-    const body = { authToken: jwtToken, processPayload: true };
+    // tell SIP that the client supports S3 payload processing
+    const body = { authToken: jwtToken, allowS3UploadedPayload: true };
+
     const authHeader = makeAuthorizationHeader(config, 'scopeRequest/authCode', 'POST', body);
     const contentLength = Buffer.byteLength(JSON.stringify(body));
     const headers = {
